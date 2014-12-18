f03b3b46c2d537d17ef97fc4038006420255e84f
Nick <nickjoannidi@gmail.com>
2014-12-18 10:35:37 -0500

    Dirname

--- bin/dbHandler.js
+++ bin/dbHandler.js
@@ -5,13 +5,13 @@ var client, files, currentFile, database;
 module.exports.beginMigration = function (credentials, filesToProcess)
 {
 
-	if(!fs.existsSync('./orms/'+credentials.type+'.js'))
+	if(!fs.existsSync(__dirname+'/../orms/'+credentials.type+'.js'))
 	{	
-		process.stdout.write('\nDatabase Type: '.red + credentials.db.type + ' not supported.\n Supported types: mysql, pgsql');
+		process.stdout.write('\nDatabase Type: '.red + credentials.type + ' not supported.\n Supported types: mysql, pgsql');
 		process.exit(1);
 	}
 
-	database = require('../orms/'+credentials.type+'.js');
+	database = require(__dirname+'/../orms/'+credentials.type+'.js');
 
 	process.stdout.write('\nConnecting as '.green + credentials.username + ' ... ');
 
