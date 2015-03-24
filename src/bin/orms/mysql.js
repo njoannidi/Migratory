2beff3d1756e48b456f10ae59b2acdd9b5105fc7
Nick <nickjoannidi@gmail.com>
2015-03-24 14:11:24 -0400

    mysql -> coffee

--- src/bin/orms/mysql.js
+++ src/bin/orms/mysql.js
@@ -19,14 +19,13 @@ errorHandler = require('../bin/errorHandler.js');
       rollback
       commit
       processFile
-      handleError
 
    The following methods are OPTIONAL:
       setSchema
  */
 
 mysqlDatabaseHandler = {
-  connect: function(credentials, cb) {
+  connect: function(credentials, success, failure) {
     var connection;
     connection = mysql.createConnection({
       host: credentials.host
@@ -37,54 +36,54 @@ mysqlDatabaseHandler = {
       multipleStatements: true
     });
     return connection.connect(function(err) {
-      if (err) {
-        throw err;
+      if (err && failure) {
+        return failure(err, connection, this);
       }
       process.stdout.write('Successful'.green + '\n');
-      if (cb) {
-        return cb(connection);
+      if (success) {
+        return success(connection);
       }
     });
   },
-  setSchema: function(client, schema, cb) {
+  setSchema: function(client, schema, success, failure) {
     console.log('\nSchemas are not supported in MySQL... Moving on. (Schema for this connection set to: '.green + credentials.schema + ')'.green);
-    if (cb) {
-      return cb(client);
-    }
+    return success(client);
   },
-  beginTransaction: function(client, cb) {
+  beginTransaction: function(client, success, failure) {
     return client.query('START TRANSACTION', function(err, results) {
-      if (err) {
-        throw err;
+      if (err && failure) {
+        return failure(err, client, this);
       }
-      if (cb) {
-        return cb(client);
+      if (success) {
+        return success(client);
       }
     });
   },
   rollback: function(client, success, failure) {
-    return client.query('ROLLBACK', function(err, results) {}, err && failure ? failure(client) : void 0, success ? success(client) : void 0);
-  },
-  commit: function(client, cb) {
-    return client.query('COMMIT', function(err, result) {
-      if (err) {
-        throw err;
+    return client.query('ROLLBACK', function(err, results) {
+      if (err && failure) {
+        return failure(err, client, this);
       }
-      if (cb) {
-        return cb(client);
+      if (success) {
+        return success(client);
       }
     });
   },
-  processFile: function(client, sqlFile, cb) {
+  commit: function(client, success, failure) {
+    return client.query('COMMIT', function(err, result) {
+      if (err && failure) {
+        return failure(err, client, this);
+      }
+      return success(client);
+    });
+  },
+  processFile: function(client, sqlFile, success, failure) {
     return client.query(sqlFile, function(err, result) {
-      if (err) {
-        throw err;
+      if (err && failure) {
+        failure(err, client, this);
       }
       return process.stdout.write(' Successful'.green);
-    }, cb ? cb(client) : void 0);
-  },
-  handleError: function(err, client) {
-    return errorHandler.handleDbError(err, client, this);
+    }, success(client));
   }
 };
 
